08c9ca1a6115141ca8d5a68b60f2b05f55037b9c
Tux <write@imaginarycode.com>
2017-02-26 11:23:31 -0500

    Dank server name list 2017 edition

--- src/main/java/io/minimum/servinator/Servinator.java
+++ src/main/java/io/minimum/servinator/Servinator.java
@@ -9,14 +9,8 @@ import java.net.InetSocketAddress;
 
 public class Servinator {
     public static void main(String... args) throws Exception {
-        if (args.length != 1) {
-            System.out.println("java -jar ... <host:port>");
-            return;
-        }
-
-        InetSocketAddress socketAddress = getAddr(args[0]);
-
-        Server server = new Server(socketAddress);
+        String port = System.getenv("PORT");
+        Server server = new Server(port == null ? 8080 : Integer.parseInt(port));
 
         HandlerList handlerList = new HandlerList();
         handlerList.addHandler(new GenerateNameHandler());
@@ -24,16 +18,5 @@ public class Servinator {
 
         server.setHandler(handlerList);
         server.start();
-    }
-
-    private static InetSocketAddress getAddr(String hostline)
-    {
-        String[] split = hostline.split( ":" );
-        int port = 8000;
-        if ( split.length > 1 )
-        {
-            port = Integer.parseInt( split[1] );
-        }
-        return new InetSocketAddress( split[0], port );
     }
 }
